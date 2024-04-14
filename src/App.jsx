dd8182dbb386f93c90a9d89e66ee912c56225d24
Nygil Pereira <nygilgp@gmail.com>
2024-04-14 13:06:51 +0530

    initialize lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,5 @@
 import React from 'react';
-import { SplitScreen } from './SplitScreen';
-
-const LeftSideComp = ({ title }) => {
-  return <h2 style={{ backgroundColor: 'crimson' }}>{title}</h2>;
-};
-
-const RightSideComp = ({ title }) => {
-  return <h2 style={{ backgroundColor: 'burlywood' }}>{title}</h2>;
-};
 
 export const App = () => {
-  return (
-    <SplitScreen leftWidth={1} rightWidth={3}>
-      <LeftSideComp title="Left!" />
-      <RightSideComp title="Right!!" />
-    </SplitScreen>
-  );
+  return <></>;
 };
