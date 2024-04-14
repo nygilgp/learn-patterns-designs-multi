3f377894f464a1796f6d4a83b2fc95b5f45eb0ee
Nygil Pereira <nygilgp@gmail.com>
2024-04-14 19:26:06 +0530

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import { authors } from './data/authors';
-import { books } from './data/books';
-import SmallAuthorsListItems from './components/authors/SmallListItems';
-import LargeAuthorsListItems from './components/authors/LargeListItems';
-import Regular from './components/lists/Regular';
-import SmallBookListItems from './components/books/SmallListItems';
-import LargeBookListItems from './components/books/LargeListItems';
-import Numberd from './components/lists/Numberd';
-import Modal from './components/Modal';
 
 export const App = () => {
   return (
     <>
-      <Modal>
-        <LargeBookListItems book={books[0]} />
-      </Modal>
+      <h1>Start Here!</h1>
     </>
   );
 };
