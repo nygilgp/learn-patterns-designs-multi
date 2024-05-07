dbf158d275525275bcb5c133794fa7d857e4e178
Nygil Pereira <nygilgp@gmail.com>
2024-05-07 10:07:10 +0530

    Make a link look like button, but work like link

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react';
+import Button from './components/button';
+
+const style = {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1rem',
+  alignItems: 'center',
+};
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>Start here...</div>
-    </>
+    <div style={style}>
+      <Button size="s">Small</Button>
+      <Button size="m">Medium</Button>
+      <Button size="l">Large</Button>
+      <Button size="xl">xLarge</Button>
+      <Button As="a" size="l" href="/">
+        Link
+      </Button>
+    </div>
   );
 }
 
