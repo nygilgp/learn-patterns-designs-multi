55894e20b70dd708d4bbbd672300e08a35be78f3
Nygil Pereira <nygilgp@gmail.com>
2024-10-11 00:40:31 +0530

    api layer changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react';
+import Users from "./components/users";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>Start here...</div>
+      <Users />
     </>
   );
 }
