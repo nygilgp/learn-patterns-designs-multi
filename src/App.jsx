1b19f462319e2c347cd1d29876d88a8587d19c6a
Nygil Pereira <nygilgp@gmail.com>
2024-10-11 17:30:04 +0530

    basic updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
+import React from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  // return React.createElement('h1', { id: 'hello' }, 'hi there...');
+  // return <h1 id="hello">Hi there...</h1>;
+  const label = <label htmlFor="inputId">Name</label>;
   return (
-    <>
-      <div>Start here...</div>
-    </>
+    <div className="large" id="largeDiv">
+      {label}
+      <input id="inputId" value={3} type="number" />
+    </div>
   );
 }
 
