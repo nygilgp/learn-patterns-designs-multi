23922bcca98ed28775736dfbfeb5876eb71414b1
Nygil Pereira <nygilgp@gmail.com>
2024-10-13 15:07:13 +0530

    learned colors, customization and typography

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,27 @@ function App() {
 
   return (
     <>
-      <div>Start here...</div>
+      <h1 className="bg-green-600 text-white">Hello world!</h1>
+      <h1 className="border-4 border-blue-600">Hello world!</h1>
+      <h1 className="text-vuejs">Hello world!</h1>
+      <div className="my-component">
+        <p className="text-xs mb-4 sm:text-base md:text-lg lg:text-3xl">
+          Some short paragraph
+        </p>
+        <p className="bg-[#bada55] truncate">
+          Am another paragraph, using an inline background style class.
+        </p>
+      </div>
+      <div>
+        <h1 className="text-3xl">Title 1</h1>
+        <h2 className="text-2xl">Title 2</h2>
+        <h3 className="text-xl italic underline decoration-red-500 decoration-wavy underline-offset-4 decoration-3">
+          Title 3
+        </h3>
+        <p className="text-base">A regular paragraph</p>
+        <p className="text-sm">A description paragraph</p>
+        <p className="note text-xs">A little note</p>
+      </div>
     </>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    spacing: {
+      1: '8px',
+      2: '12px',
+      3: '16px',
+      4: '24px',
+      5: '32px',
+      6: '48px',
+    },
+    extend: {
+      colors: {
+        vuejs: '#53ed7c',
+      },
+      fontSize: {
+        base: '18px',
+      },
+    },
+  },
+  plugins: [],
+};
